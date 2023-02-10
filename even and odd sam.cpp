25928d508ff93a1dcf116a7a45c6f4762cd30d29
Sachin-Mhetre <118711683+Sachin-Mhetre@users.noreply.github.com>
2023-02-10 09:46:47 +0530

    Add files via upload

---
+++ even and odd sam.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i ;
+	printf("Enter a number :");
+	scanf("%d",&i);
+	if (i%2 == 0)
+	{
+		printf("Its a even number");
+	}
+	else{
+		printf("Its a odd number");
+	}
+}
